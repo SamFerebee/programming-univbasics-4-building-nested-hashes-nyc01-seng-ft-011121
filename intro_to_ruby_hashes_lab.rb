dd923ed5197fbeda7769036b6b687c339d348e9e
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-05 18:19:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   
    hash = {
       :railroads => {
-        :pieces => 4, :rent_in_dollars => {:one_pieces_owned => 25, :two_piece_owned => 50}, :names => {}
+        :pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50}, :names => {}
         
       }
     }
